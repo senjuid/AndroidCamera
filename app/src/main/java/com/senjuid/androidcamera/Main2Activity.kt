e6648c6555b987285a7f8753e52ae8b28d8219d0
Hendi Hidayat <hendi.evolution@gmail.com>
2020-03-24 19:17:36 +0700

    Migrate androidX

--- app/src/main/java/com/senjuid/androidcamera/Main2Activity.kt
+++ app/src/main/java/com/senjuid/androidcamera/Main2Activity.kt
@@ -3,7 +3,7 @@ package com.senjuid.androidcamera
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import com.senjuid.camera.CaptureActivity
 import kotlinx.android.synthetic.main.activity_main2.*
--- camera/src/main/java/com/senjuid/camera/CaptureActivity.kt
+++ camera/src/main/java/com/senjuid/camera/CaptureActivity.kt
@@ -8,7 +8,7 @@ import android.graphics.Matrix
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Environment
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.view.WindowManager
 import com.otaliastudios.cameraview.CameraListener
