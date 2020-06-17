3e8c1cca9d401dd6429185a885a8f24c10c5aea4
Hendi Hidayat <hendi.evolution@gmail.com>
2020-06-17 11:26:17 +0700

    Add capture activity helper

--- camera/src/main/java/com/senjuid/camera/CaptureActivity.kt
+++ camera/src/main/java/com/senjuid/camera/CaptureActivity.kt
@@ -3,11 +3,8 @@ package com.senjuid.camera
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.Matrix
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.Environment
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -17,14 +14,6 @@ import com.otaliastudios.cameraview.controls.Facing
 import com.otaliastudios.cameraview.controls.Flash
 import com.otaliastudios.cameraview.size.SizeSelectors
 import kotlinx.android.synthetic.main.activity_capture.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.util.*
 
 
 /**
@@ -33,11 +22,9 @@ import java.util.*
 class CaptureActivity : AppCompatActivity(), RunTimePermission.RunTimePermissionListener {
 
     private var runTimePermission: RunTimePermission? = null
-    private var folder: File? = null
-    private var photo: String = "img_default"
     private var countDownTimer: CountDownTimer? = null
     private lateinit var muteController: MuteController
-    private var bitmapResult: Bitmap? = null
+    private lateinit var helper: CaptureActivityHelper
 
     // MARK: Lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,15 +32,21 @@ class CaptureActivity : AppCompatActivity(), RunTimePermission.RunTimePermission
         setContentView(R.layout.activity_capture)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
+        helper = CaptureActivityHelper()
         muteController = MuteController(this)
 
         // Add camera listener
         camera_view.addCameraListener(object : CameraListener() {
             override fun onPictureTaken(result: PictureResult) {
-                previewResult(result)
+                var maxSize = intent.getIntExtra("max_size", 0)
+                helper.pictureResultHandler(result, maxSize) {
+                    iv_preview.setImageBitmap(it)
+                    showProgressDialog(false)
+                    viewMode(false)
+                }
             }
         })
-        camera_view.cameraOptions
+        camera_view.setLifecycleOwner(this)
         camera_view.setPictureSize(SizeSelectors.smallest())
 
         // Add take picture button listener
@@ -85,7 +78,12 @@ class CaptureActivity : AppCompatActivity(), RunTimePermission.RunTimePermission
 
         // Add select picture button listener
         btn_select_picture.setOnClickListener {
-            saveBitmapAndFinish()
+            helper.saveBitmapAndFinish(intent, camera_view.facing) {
+                val returnIntent = Intent()
+                returnIntent.putExtra("photo", it)
+                setResult(Activity.RESULT_OK, returnIntent)
+                finish()
+            }
         }
 
         btn_flash_on.setOnClickListener {
@@ -107,12 +105,8 @@ class CaptureActivity : AppCompatActivity(), RunTimePermission.RunTimePermission
         // prepare (grant permission and make directory)
         prepare()
 
-        // Get params
-        val bundle: Bundle? = intent.extras
-        photo = bundle?.getString("name")!!
-
         // check front disable front camera
-        if (bundle.getBoolean("disable_back", false)) {
+        if (intent.getBooleanExtra("disable_back", false)) {
             btn_switch_camera.visibility = View.GONE
         } else {
             btn_switch_camera.visibility = View.VISIBLE
@@ -144,17 +138,6 @@ class CaptureActivity : AppCompatActivity(), RunTimePermission.RunTimePermission
         countDownTimer?.start()
     }
 
-    public
-    override fun onResume() {
-        super.onResume()
-        camera_view.open()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        camera_view.close()
-    }
-
     override fun onStop() {
         countDownTimer?.cancel()
         super.onStop()
@@ -165,76 +148,15 @@ class CaptureActivity : AppCompatActivity(), RunTimePermission.RunTimePermission
         runTimePermission?.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    //
-    // MARK: Own methods
-    //
-    private fun previewResult(data: PictureResult) {
-        var maxSize = intent.getIntExtra("max_size", 0)
-        if (maxSize > 0) {
-            data?.toBitmap(maxSize!!, maxSize!!) {
-                iv_preview.setImageBitmap(it)
-                showProgressDialog(false)
-                viewMode(false)
-                bitmapResult = it
-            }
-        } else {
-            data?.toBitmap {
-                iv_preview.setImageBitmap(it)
-                showProgressDialog(false)
-                viewMode(false)
-                bitmapResult = it
-            }
-        }
-    }
-
-    private fun saveBitmapAndFinish() {
-        bitmapResult?.let {
-            var bmp = it
-            CoroutineScope(Dispatchers.IO).launch {
-                // Mirroring option
-                val snapshot = intent.getBooleanExtra("is_snapshot", true)
-                var disableMirror = intent.getBooleanExtra("disable_mirror", true)
-                if (camera_view.facing == Facing.FRONT && disableMirror!! && !snapshot) {
-                    bmp = it.flip(-1f, 1f, it.width / 2f, it.height / 2f)
-                }
-
-                // Save picture to sdcard
-                var compress = intent.getIntExtra("quality", 100)
-                val prefix = intent.getStringExtra("name")
-                val fileName = createFileName(prefix)
-                val file = File(folder, fileName)
-                val fileOutputStream = FileOutputStream(file)
-                bmp.compress(Bitmap.CompressFormat.JPEG, compress!!, fileOutputStream)
-
-                withContext(Dispatchers.Main) {
-                    // Finish
-                    val returnIntent = Intent()
-                    returnIntent.putExtra("photo", file?.absolutePath)
-                    setResult(Activity.RESULT_OK, returnIntent)
-                    finish()
-                }
-            }
-        }
-    }
-
-    private fun Bitmap.flip(x: Float, y: Float, cx: Float, cy: Float): Bitmap {
-        val matrix = Matrix().apply { postScale(x, y, cx, cy) }
-        return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
-    }
-
     private fun showProgressDialog(show: Boolean) {
         layout_progress.visibility = if (show) View.VISIBLE else View.GONE
     }
 
     private fun viewMode(isCapture: Boolean) {
         if (isCapture) {
-            btn_select_picture.visibility = View.GONE
-            iv_preview.visibility = View.GONE
-            btn_retake.visibility = View.GONE
+            layout_preview.visibility = View.GONE
         } else {
-            btn_select_picture.visibility = View.VISIBLE
-            iv_preview.visibility = View.VISIBLE
-            btn_retake.visibility = View.VISIBLE
+            layout_preview.visibility = View.VISIBLE
         }
     }
 
@@ -248,42 +170,11 @@ class CaptureActivity : AppCompatActivity(), RunTimePermission.RunTimePermission
                 this)
     }
 
-    private fun createFileName(prefixName: String?): String {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = if (calendar.get(Calendar.MONTH) + 1 < 10) {
-            "0" + (calendar.get(Calendar.MONTH) + 1)
-        } else {
-            "" + (calendar.get(Calendar.MONTH) + 1)
-        }
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        return if (prefixName?.isEmpty() == true) {
-            "$photo${"_"}$year$month$day${"_"}${System.currentTimeMillis()}.png"
-        } else {
-            "$prefixName${"_"}$year$month$day${"_"}${System.currentTimeMillis()}.png"
-        }
-    }
-
     //
     // MARK: RunTimePermission.RunTimePermissionListener
     //
     override fun permissionGranted() {
-        // Create directory
-        val dirPath = "${Environment.getExternalStorageDirectory().path}/GreatDayHR"
-        folder = File(dirPath)
-        if (folder?.exists() == false) {
-            folder?.mkdirs()
-
-            // Create .nomedia file
-            val noMediaPath = "${folder?.path}/.nomedia"
-            val noMediaFile = File(noMediaPath)
-            try {
-                noMediaFile.createNewFile()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
+        helper.createDirectory()
     }
 
     override fun permissionDenied() {
---
+++ camera/src/main/java/com/senjuid/camera/CaptureActivityHelper.kt
@@ -0,0 +1,101 @@
+package com.senjuid.camera
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.os.Environment
+import com.otaliastudios.cameraview.PictureResult
+import com.otaliastudios.cameraview.controls.Facing
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+
+class CaptureActivityHelper {
+    private var bitmapResult: Bitmap? = null
+    private var photo: String = "img_default"
+    private var folder: File? = null
+
+    fun createDirectory() {
+        val dirPath = "${Environment.getExternalStorageDirectory().path}/GreatDayHR"
+        folder = File(dirPath)
+        if (folder?.exists() == false) {
+            folder?.mkdirs()
+
+            // Create .nomedia file
+            val noMediaPath = "${folder?.path}/.nomedia"
+            val noMediaFile = File(noMediaPath)
+            try {
+                noMediaFile.createNewFile()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun pictureResultHandler(data: PictureResult, maxSize: Int, callback: (Bitmap?) -> Unit) {
+        if (maxSize > 0) {
+            data?.toBitmap(maxSize!!, maxSize!!) {
+                bitmapResult = it
+                callback(bitmapResult)
+            }
+        } else {
+            data?.toBitmap {
+                bitmapResult = it
+                callback(bitmapResult)
+            }
+        }
+    }
+
+    fun saveBitmapAndFinish(intent: Intent, facing: Facing, callback: (String?) -> Unit) {
+        bitmapResult?.let {
+            var bmp = it
+            CoroutineScope(Dispatchers.IO).launch {
+                // Mirroring option
+                val snapshot = intent.getBooleanExtra("is_snapshot", true)
+                var disableMirror = intent.getBooleanExtra("disable_mirror", true)
+                if (facing == Facing.FRONT && disableMirror!! && !snapshot) {
+                    bmp = it.flip(-1f, 1f, it.width / 2f, it.height / 2f)
+                }
+
+                // Save picture to sdcard
+                var compress = intent.getIntExtra("quality", 100)
+                val prefix = intent.getStringExtra("name")
+                val fileName = createFileName(prefix)
+                val file = File(folder, fileName)
+                val fileOutputStream = FileOutputStream(file)
+                bmp.compress(Bitmap.CompressFormat.JPEG, compress!!, fileOutputStream)
+
+                withContext(Dispatchers.Main) {
+                    callback(file?.absolutePath)
+                }
+            }
+        }
+    }
+
+    private fun Bitmap.flip(x: Float, y: Float, cx: Float, cy: Float): Bitmap {
+        val matrix = Matrix().apply { postScale(x, y, cx, cy) }
+        return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
+    }
+
+    private fun createFileName(prefixName: String?): String {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = if (calendar.get(Calendar.MONTH) + 1 < 10) {
+            "0" + (calendar.get(Calendar.MONTH) + 1)
+        } else {
+            "" + (calendar.get(Calendar.MONTH) + 1)
+        }
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return if (prefixName?.isEmpty() == true) {
+            "$photo${"_"}$year$month$day${"_"}${System.currentTimeMillis()}.png"
+        } else {
+            "$prefixName${"_"}$year$month$day${"_"}${System.currentTimeMillis()}.png"
+        }
+    }
+}
