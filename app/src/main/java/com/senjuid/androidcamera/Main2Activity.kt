9ed4bd45b46c7baf14d23378c9599fb6f57e4652
Hendi Hidayat <hendi.evolution@gmail.com>
2020-07-21 09:59:40 +0700

    Update app UI

--- app/src/main/java/com/senjuid/androidcamera/Main2Activity.kt
+++ app/src/main/java/com/senjuid/androidcamera/Main2Activity.kt
@@ -1,5 +1,6 @@
 package com.senjuid.androidcamera
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,8 @@ import com.senjuid.camera.CameraPlugin
 import com.senjuid.camera.CameraPluginListener
 import com.senjuid.camera.CameraPluginOptions
 import kotlinx.android.synthetic.main.activity_main2.*
+import java.io.File
+
 
 class Main2Activity : AppCompatActivity() {
 
@@ -19,7 +22,7 @@ class Main2Activity : AppCompatActivity() {
         cameraPlugin = CameraPlugin(this)
         cameraPlugin?.setCameraPluginListener(object : CameraPluginListener {
             override fun onSuccess(photoPath: String) {
-                Toast.makeText(this@Main2Activity, photoPath, Toast.LENGTH_LONG).show()
+                showImage(photoPath)
             }
 
             override fun onCancel() {
@@ -28,14 +31,30 @@ class Main2Activity : AppCompatActivity() {
         })
 
         button_click.setOnClickListener {
-            val quality = et_quality.text.toString().toInt()
-            val maxSize = et_max_size.text.toString().toInt()
+            val quality = et_quality.text.toString()
+            val maxSize = et_max_size.text.toString()
+            if (quality.isNullOrEmpty()) {
+                Toast.makeText(this@Main2Activity, "Please input image quality (1 - 100).", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            if (maxSize.isNullOrEmpty()) {
+                Toast.makeText(this@Main2Activity, "Please input image maximum size.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
 
             val options = CameraPluginOptions.Builder()
-                    .setMaxSize(maxSize)
-                    .setQuality(quality)
+                    .setMaxSize(maxSize.toInt())
+                    .setQuality(quality.toInt())
                     .build()
             cameraPlugin?.open(options)
+        }
+    }
+
+    fun showImage(imagePath: String) {
+        val imgFile = File(imagePath)
+        if (imgFile.exists()) {
+            val bitmap = BitmapFactory.decodeFile(imgFile.absolutePath)
+            iv_preview.setImageBitmap(bitmap)
         }
     }
 
